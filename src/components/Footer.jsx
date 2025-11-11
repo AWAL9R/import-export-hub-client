62de3793750341ddc7fb1777ecceef1a2bab71ce
unknown <awal9r@gmail.com>
2025-11-11 14:49:13 +0600

    Footer design completed, product details page added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { FaFacebook, FaLinkedin, FaX } from 'react-icons/fa6';
+import { MdEmail } from 'react-icons/md';
 
 const Footer = () => {
     return (
         <div className='bg-accent-content text-white mt-10 py-5 px-10'>
             <div className='flex flex-wrap justify-between gap-5'>
-                <div className='max-w-150'>
+                <div className='max-w-150 space-y-3'>
                     <h2>ExportMania</h2>
-                    <p className='text-gray-200'>
+                    <p className='text-gray-200 text-justify'>
                         ExportMania.app is a modern platform designed to simplify global trade management. It helps businesses efficiently manage exports, track products, and connect with buyers worldwide. With user-friendly tools for inventory, pricing, and documentation, ExportMania empowers exporters to grow faster and operate smarter in today’s competitive international market.
                     </p>
                 </div>
@@ -24,12 +26,12 @@ const Footer = () => {
                     <a href="">Join Us</a>
                 </div>
 
-                <div className='flex flex-col space-y-3'>
+                <div className='flex flex-col space-y-3 *:flex *:gap-2 *:items-center   '>
                     <h2>Social Links</h2>
-                    <a href="">@SkillCircle.Edu</a>
-                    <a href="">@SkillCircle.Edu</a>
-                    <a href="">@SkillCircle.Edu</a>
-                    <a href="">support@SkillCircle.Edu</a>
+                    <a href=""><FaX/> @SkillCircle.Edu</a>
+                    <a href=""><FaFacebook/> @SkillCircle.Edu</a>
+                    <a href=""><FaLinkedin/> @SkillCircle.Edu</a>
+                    <a href=""><MdEmail/> support@SkillCircle.Edu</a>
                 </div>
             </div>
             <div className="border-t border-t-gray-400 text-center pt-5 my-3">
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router';
 
 const ProductPage = () => {
+    const loaderData=useLoaderData();
+    const [product, setProduct]=useState(loaderData)
+    
     return (
-        <div>
-            Product Page
+        <div className='my-10'>
+            <div className="container bg-white rounded-md overflow-hidden">
+                <div className='flex flex-wrap gap-3'>
+                    <img src={product.photo} alt="" className='aspect-aquare w-1/3 max-lg:w-1/2 max-md:w-full object-cover  mb-3'/>
+                    <div className='py-5'>
+                        <h2>
+                            {product.name}
+                        </h2>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
